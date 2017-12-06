6a19dda4b1f81d8a9980a06cdf09b53c57b2824c
OloinMilsom <keporagebora@gmail.com>
2017-12-06 21:06:13 +0000

    Day06

---
+++ Day06_1.py
@@ -0,0 +1,23 @@
+input = [11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11]
+
+def balance_list(int_list):
+    maximum = max(int_list)
+    max_index = int_list.index(maximum)
+    int_list[max_index] = 0
+    for i in range(maximum):
+        curr_index = max_index + i + 1
+        while curr_index >= len(int_list):
+            curr_index -= len(int_list)
+        int_list[curr_index] += 1
+    return int_list
+
+curr = input
+visited = {}
+steps = 0
+
+while str(curr) not in visited:
+    visited[str(curr)] = steps
+    curr = balance_list(curr)
+    steps += 1
+
+print(steps - visited[str(curr)])
